0a18e765540ac0ebf471dc2a4b9462f143403e00
Alice Chen <alicechen118@gmail.com>
2017-08-05 17:18:59 -0700

    Redo routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  get 'session/create'
-
-  get 'session/destroy'
-
-  get 'users/show'
-
-  get 'users/create'
-
-  get 'users/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show, :create, :destroy]
+  resource :session, only: [:create, :destroy]
 end
