f2e8a2b78abf01760f58b20f46b77c0552ccc21d
Alice Chen <alicechen118@gmail.com>
2017-08-05 17:20:47 -0700

    Set up users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,29 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find_by(id: params[:id])
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
   end
 
   def destroy
+    @user = User.find_by(id: params[:id])
+    if @user.destroy
+      render json: {}
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
