a8c66cd0041a4f2dcbf03fc39316495b6463ace1
Alice Chen <alicechen118@gmail.com>
2017-08-05 17:03:55 -0700

    Create users table and migrate

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'session/create'
+
+  get 'session/destroy'
+
+  get 'users/show'
+
+  get 'users/create'
+
+  get 'users/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
