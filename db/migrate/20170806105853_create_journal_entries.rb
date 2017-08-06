cad54d350a57bea4d7e7bcc2404e7307bfae92c2
Alice Chen <alicechen118@gmail.com>
2017-08-06 04:30:09 -0700

    Fix array column in journal entries table

--- db/migrate/20170806105853_create_journal_entries.rb
+++ db/migrate/20170806105853_create_journal_entries.rb
@@ -2,7 +2,7 @@ class CreateJournalEntries < ActiveRecord::Migration[5.1]
   def change
     create_table :journal_entries do |t|
       t.integer :user_id, null: false
-      t.array :entry, null: false
+      t.text :entry, array: true, default: [], null: false
       t.timestamps
     end
     add_index :journal_entries, :user_id
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806105030) do
+ActiveRecord::Schema.define(version: 20170806105853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "journal_entries", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "entry", default: [], null: false, array: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_journal_entries_on_user_id"
+  end
 
   create_table "measurements", force: :cascade do |t|
     t.integer "user_id", null: false
