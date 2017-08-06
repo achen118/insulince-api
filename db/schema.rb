29b30ab24aecfad370976d2ebbe4d6755b4160b7
Alice Chen <alicechen118@gmail.com>
2017-08-06 03:55:48 -0700

    Create measurements table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170805235648) do
+ActiveRecord::Schema.define(version: 20170806105030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "measurements", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "chest"
+    t.integer "waist"
+    t.integer "hips"
+    t.integer "weight"
+    t.integer "height"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_measurements_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
