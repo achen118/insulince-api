40d4dee6335e028cc9079592bacde42f018e9c8e
Alice Chen <alicechen118@gmail.com>
2017-08-10 18:45:21 -0700

    Remove login json error

--- app/controllers/api/user_token_controller.rb
+++ app/controllers/api/user_token_controller.rb
@@ -1,16 +1,12 @@
 module Api
   class UserTokenController < Knock::AuthTokenController
     def create
-      if current_user
-        render json: {
-          auth_token: auth_token.token,
-          id: entity.id,
-          username: entity.username.downcase,
-          email: entity.email.downcase
-        }, status: :created
-      else
-        render json: ["Invalid credentials"], status: 422
-      end
+      render json: {
+        auth_token: auth_token.token,
+        id: entity.id,
+        username: entity.username.downcase,
+        email: entity.email.downcase
+      }, status: :created
     end
 
     def auth_params
