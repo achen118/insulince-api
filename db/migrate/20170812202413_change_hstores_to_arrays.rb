83472dae1902de0bda604a1a6f1d80e7777add66
Alice Chen <alicechen118@gmail.com>
2017-08-12 14:00:46 -0700

    Migrate db

--- db/migrate/20170812202413_change_hstores_to_arrays.rb
+++ db/migrate/20170812202413_change_hstores_to_arrays.rb
@@ -4,9 +4,9 @@ class ChangeHstoresToArrays < ActiveRecord::Migration[5.1]
     remove_column :journal_entries, :lunch, :hstore
     remove_column :journal_entries, :dinner, :hstore
     remove_column :journal_entries, :snacks, :hstore
-    add_column :journal_entries, :breakfast, array: true, default: []
-    add_column :journal_entries, :lunch, array: true, default: []
-    add_column :journal_entries, :dinner, array: true, default: []
-    add_column :journal_entries, :snacks, array: true, default: []
+    add_column :journal_entries, :breakfast, :text, array: true, default: []
+    add_column :journal_entries, :lunch, :text, array: true, default: []
+    add_column :journal_entries, :dinner, :text, array: true, default: []
+    add_column :journal_entries, :snacks, :text, array: true, default: []
   end
 end
